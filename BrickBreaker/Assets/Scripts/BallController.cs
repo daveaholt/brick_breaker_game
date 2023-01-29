7b116ade743a229204d719d14268f33e9f1aff2a
Dave Aholt <daveaholt@gmail.com>
2023-01-29 13:36:39 -0600

    Menu and level loading correctly

--- BrickBreaker/Assets/Scripts/BallController.cs
+++ BrickBreaker/Assets/Scripts/BallController.cs
@@ -1,34 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallController : MonoBehaviour
 {
     Rigidbody2D rb;
     public float bounceForce;
+    private int bounceCount;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        bounceCount = 0;
     }
 
     void Start()
     {
-        
+        StartBounce();
     }
 
     
     void Update()
     {
-        if (Input.anyKeyDown)
-        {
-            StartBounce();
-        }
     }
 
     void StartBounce()
     {
         var randomDirection = new Vector2(Random.Range(-1, 1), 1);
         rb.AddForce(randomDirection * bounceForce, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "FallCheck")
+        {
+            SceneManager.LoadScene("Menu");
+        }
+        else
+        {
+            bounceCount++;
+            if( bounceCount > 9)
+            {
+                bounceForce = bounceForce + 10;
+                var forceVector = new Vector2(rb.velocity.x, rb.velocity.y);
+                rb.AddForce(forceVector * bounceForce);
+                bounceCount = 0;
+            }
+        }
     }
 }
